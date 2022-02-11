88022bc3ebc65d095d5c1d84cc50df8e5a378390
May Hemade <71593691+May-Hemade@users.noreply.github.com>
2022-02-11 12:39:12 +0200

    connect to heroku

--- src/utils/db/connect.js
+++ src/utils/db/connect.js
@@ -1,22 +1,42 @@
-import Sequelize from "sequelize";
+import Sequelize from "sequelize"
 
-const {POSTGRES_URI } = process.env;
+const { DATABASE_URL } = process.env
 
-const sequelize = new Sequelize(POSTGRES_URI, {
+// const sequelize = new Sequelize(DATABASE_URL, {
+//   dialect: "postgres",
+// });
+
+let options = {
+  port: process.env.PORT,
+  host: process.env.HOST,
   dialect: "postgres",
-});
+}
 
+if (process.env.REQUIRE_SSL === "true") {
+  options.dialectOptions = {
+    ssl: {
+      require: true,
+      rejectUnauthorized: true,
+    },
+  }
+}
 
+const sequelize = new Sequelize(
+  process.env.DATABASE,
+  process.env.USER,
+  process.env.PASSWORD,
+  options
+)
 
 export const authenticateDatabase = async () => {
   try {
-    await sequelize.authenticate({ logging: false });
-    await sequelize.sync({ alter: true, logging: false });
-    console.log("Connection has been established successfully.");
+    await sequelize.authenticate({ logging: false })
+    await sequelize.sync({ alter: true, logging: false })
+    console.log("Connection has been established successfully.")
   } catch (error) {
-    console.log(error);
-    console.error( "Unable to connect to the database:", error);
+    console.log(error)
+    console.error("Unable to connect to the database:", error)
   }
-};
+}
 
-export default sequelize;
+export default sequelize
